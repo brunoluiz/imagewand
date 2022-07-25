5bfe684e271c21563e4a6f4b621188fbf5d8e2f3
Bruno Luiz Silva <contact@brunoluiz.net>
2022-07-25 22:48:44 +0100

    chore: remove unused convertFromURL (optimises binary)

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -3,43 +3,11 @@ package main
 import (
 	"bytes"
 	"errors"
-	"net/http"
 	"syscall/js"
 
 	"github.com/brunoluiz/imagewand"
 	"github.com/brunoluiz/imagewand/jasm"
 )
-
-func convertFromURL() js.Func {
-	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		return jasm.Await(func() (js.Value, error) {
-			if len(args) != 2 {
-				return js.Value{}, errors.New("please specify format and blob")
-			}
-
-			format := args[0].String()
-			url := args[1].String()
-
-			res, err := http.DefaultClient.Get(url)
-			if err != nil {
-				return js.Value{}, err
-			}
-			defer res.Body.Close()
-
-			img, err := imagewand.New(res.Body)
-			if err != nil {
-				return js.Value{}, err
-			}
-
-			b := bytes.NewBuffer([]byte{})
-			if err := img.Convert(b, imagewand.FileFormat(format)); err != nil {
-				return js.Value{}, err
-			}
-
-			return jasm.ArrayBuffer(b.Bytes()), nil
-		})
-	})
-}
 
 func convertFromBlob() js.Func {
 	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
@@ -68,7 +36,6 @@ func convertFromBlob() js.Func {
 
 func main() {
 	js.Global().Set("wand", map[string]interface{}{
-		"convertFromURL":  convertFromURL(),
 		"convertFromBlob": convertFromBlob(),
 	})
 	<-make(chan bool)
